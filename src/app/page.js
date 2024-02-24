0e9989efb0b363d625d3e50c2ac173234d4070ed
AKHIL-DyC <akhiljee666@gmail.com>
2024-02-25 05:06:30 +0530

    v9

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./components/navbar/Navbar";
+import Link from "next/link";
 import cancer from "/public/cancer.avif"
 import orphan from "/public/orphan.avif"
 import hospital from "/public/hospital.avif"
@@ -13,7 +14,7 @@ export default function Home() {
         <div className={styles.left}><Image src={logo}></Image></div>
         <div className={styles.right}>
           <div className={styles.text}>
-            <h2>We make <span className={styles.green}>transparency</span> meet impact .</h2>
+            <h1 className={styles.heading}>We make <span className={styles.green}>transparency</span> meet impact .</h1>
             <h6 className={styles.about}>Are you an NGO or event organizer striving to make a difference but find yourself struggling with opaque funding processes and limited resources? Look no further. OpenFund is here to revolutionize the way you secure funding for your impactful events.</h6>
           </div>
         </div>
@@ -24,6 +25,7 @@ export default function Home() {
           <h2>Info Space</h2>
         </div>
         <div className={styles.cardContainer}>
+          <Link href={'./mcc'}>
           <div className={styles.card}>
             <div className={styles.cleft}>
               <Image src={cancer} className={styles.img}></Image>
@@ -34,6 +36,8 @@ export default function Home() {
                 </p>
             </div>
           </div>
+          </Link>
+          <Link href={'./mcc'}>
           <div className={styles.card}>
             <div className={styles.cleft}>
               <Image src={orphan} className={styles.img}></Image>
@@ -44,6 +48,8 @@ export default function Home() {
 The Kochi Trust is a charitable organization dedicated to supporting orphanages, old age homes, and various other institutions by providing financial aid and essential utilities. Through their philanthropic efforts, they strive to enhance the well-being and quality of life for vulnerable individuals across Kochi.</p>
             </div>
           </div>
+          </Link>
+          <Link href={'./mcc'}>
           <div className={styles.card}>
             <div className={styles.cleft}>
               <Image src={hospital} className={styles.img}></Image>
@@ -53,6 +59,7 @@ The Kochi Trust is a charitable organization dedicated to supporting orphanages,
                 <p className={styles.para}>John, a hardworking laborer, urgently requires 30 lakhs for surgery following a recent accident. As a dedicated worker, he seeks financial support to undergo the necessary medical treatment and regain his health and livelihood.</p>
             </div>
           </div>
+          </Link>
         </div>
       </div>
     </main>
